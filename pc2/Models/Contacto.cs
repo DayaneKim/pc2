b81cf154484aa93848bfe521758f1a6a11562923
Dayane Quispe <dayane_quispe@usmp.pe>
2021-09-14 18:30:07 -0500

    first commit

---
+++ pc2/Models/Contacto.cs
@@ -0,0 +1,14 @@
+namespace pc2.Models
+{
+    public class Contacto
+    {
+        public string Nombre { get; set; }
+          public string Apellidop { get; set; }
+          public string Apellidom { get; set; }
+            public string Mascota { get; set; }
+             public string Descripcion { get; set;}
+             public string Telefono { get; set;}
+              public string Distrito { get; set;}
+               public string Dirrecion { get; set;}
+    }
+}
